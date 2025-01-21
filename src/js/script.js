60b0bdd7485310b7ab6740ab83664a0d57973a39
Vladimir Seropol <frontender23@mail.ru>
2025-01-21 13:46:22 +0300

    edit1

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,2 @@
-function showScreen(screenNumber) {
-    document.querySelectorAll("div[id^='screen-']").forEach((screen) => {
-      screen.classList.add("hidden");
-    });
-    document.getElementById(`screen-${screenNumber}`).classList.remove("hidden");
-  }
+
   
